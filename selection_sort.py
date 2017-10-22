d0348c2cbbf5e58f7db1455ec67e5df8da818bb5
Arul Murugan <amurugan@sc-m-amurugan.local>
2017-10-21 22:13:46 -0700

    selection sort python program

---
+++ selection_sort.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 21 21:08:36 2017
+selection sort
+@author: Arul Perumal
+"""
+
+a = [54,26,93,17,77,31,44,55,20]
+
+list_length = len(a)
+
+for i in xrange(list_length,0,-1):
+    pos = 0
+    found = False
+    bign = 0
+    for j in xrange(i):
+        #check if current number is big? If so, update position and bignumber
+        if a[j] > bign:
+            pos = j
+            bign = a[j]
+            found = True
+    if (found):
+        a[pos],a[i-1] = a[i-1],a[pos]
+    print a        
+    
